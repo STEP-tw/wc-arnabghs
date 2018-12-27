67e0e0627f185540a418eb48e0cc3c445d3ed04e
Arnab Ghosh <44019889+arnabghs@users.noreply.github.com>
2018-12-27 17:40:23 +0530

    removed a unnecessary line from parse.js

--- src/parse.js
+++ src/parse.js
@@ -3,13 +3,12 @@ const hasOption = function(arg) {
 };
 
 const readUserInput = function(userArgs) {
-  let paths = userArgs.slice(0);
   let optionArgs = userArgs.filter(hasOption);
   return handleOptions(userArgs, optionArgs);
 };
 
 const handleOptions = function(userArgs, optionArgs) {
-  paths = userArgs.slice(optionArgs.length);
+  let paths = userArgs.slice(optionArgs.length);
   let options = optionArgs.map(x => x.substr(1));
   if (optionArgs.length == 1) options = options[0].split("");
   return { paths, options };
